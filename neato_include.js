76ff85bdd7fd8b4b258dd8b829ab2caad15c9f08
Lloyd Lentz <cledwyn@gmail.com>
2025-01-07 16:28:05 -0600

    Update neato_include.js

--- neato_include.js
+++ neato_include.js
@@ -1,4 +1,2 @@
-//$(document).ready(function() {
-  console.log("Why are people from Norway so good at editing files in Linux?");
-  console.log("Their ancestors are vi-kings!");
-//});
+console.log("Why are people from Norway so good at editing files in Linux?");
+console.log("Their ancestors are vi-kings!");
